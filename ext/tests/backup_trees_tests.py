b4f445e48d605145703da2bd229334e0f5f558b0
gyllstar <dpg@cs.umass.edu>
2013-10-01 15:53:48 -0400

    new mtree files and doc files

--- ext/tests/backup_trees_tests.py
+++ ext/tests/backup_trees_tests.py
@@ -1,6 +1,8 @@
 """
 
-Unit tests for backup_trees
+Unit tests for multicast.py working with backup trees.
+
+To run this test I had to add the appleseed root folder to the Python path
 
 @author: dpg
 """
@@ -8,26 +10,104 @@ Unit tests for backup_trees
 import unittest
 #from pox.ext.backup_trees import BackupTreeInstaller
 
-#import BackupTreeInstaller
-from trees import BackupTreeInstaller,PrimaryTree,Tree
-
-#from pox.ext.backup_trees import BackupTreeInstallers 
+from ext.multicast import PrimaryTree,BackupTree
+from pox.lib.addresses import IPAddr
 
 
-class BackupTreeInstallerTests(unittest.TestCase):
+class BackupTreeTests(unittest.TestCase):
   
-  installer = BackupTreeInstaller()
   
   def setUp(self):
     unittest.TestCase.setUp(self)
-    self.installer = BackupTreeInstaller()
   
-  def test_find_nodes_to_signal(self):
+  def test_bottom_up_signal_simple_path(self):
+    
+    # just a simple path
+    primary_edges = [(1,2),(2,3),(3,4),(4,5),(5,6)]
+    backup_edges = [(1,2),(2,7),(7,8),(8,5),(5,6)]
+  
+    
+    root = IPAddr("10.0.0.1")
+    terminals = [IPAddr("10.0.0.6")]
+    data = {"edges":primary_edges,"mcast_address":IPAddr("10.0.0.6"),"root":root,"terminals":terminals,"adjacency":None,"controller":None}   
+    primary = PrimaryTree(**data)
+    
+
+    data = {"primary_tree":primary,"edges":backup_edges,"backup_edge":(2,3),"root":root,"terminals":terminals,"adjacency":None,"controller":None,"mcast_address":IPAddr("10.0.0.7")}    
+    backup = BackupTree(**data)
+    
+    primary.backup_trees.append(backup)
+
+    #result = backup.compute_nodes_to_signal((2,3))
+    
+    expected_result = [8,7,2]
+    
+    print "test_bottom_up_signal_simple_path() result = %s" %(backup.nodes_to_signal)
+    self.assertEquals(backup.nodes_to_signal, expected_result)
+
+  def test_bottom_up_signal_trees1(self):
+    
+    # just a simple path
+    primary_edges =  [(1,5),(5,6),(6,7),(6,8),(8,9),(8,10),(7,2),(9,3),(10,4)]
+    backup_edges = [(1,5),(5,11),(11,12),(11,7),(7,2),(12,10),(12,9),(10,4),(9,3)]
+  
+    
+    root = IPAddr("10.0.0.1")
+    terminals = [IPAddr("10.0.0.2"),IPAddr("10.0.0.3"),IPAddr("10.0.0.4")]
+    data = {"edges":primary_edges,"mcast_address":IPAddr("10.10.10.10"),"root":root,"terminals":terminals,"adjacency":None,"controller":None}   
+    primary = PrimaryTree(**data)
+    
+
+    data = {"primary_tree":primary,"edges":backup_edges,"backup_edge":(1,2),"root":root,"terminals":terminals,"adjacency":None,"controller":None,"mcast_address":IPAddr("10.0.0.7")}    
+    backup = BackupTree(**data)
+    
+    primary.backup_trees.append(backup)
+
+    #result = backup.compute_nodes_to_signal((2,3))
+    
+    expected_result = [12,11,5]
+    
+    print "test_bottom_up_signal_trees1() result = %s" %(backup.nodes_to_signal)
+    print backup.nodes_to_signal
+    self.assertEquals(backup.nodes_to_signal, expected_result)
+
+    
+  def test_bottom_up_signal_trees2(self):
+    
+    # just a simple path
+    primary_edges = [(1,2),(2,3),(3,4),(3,5),(4,6),(5,7),(5,8)]
+    backup_edges = [(1,9),(9,10),(9,4),(10,7),(10,8),(4,6)]
+  
+    
+    root = IPAddr("10.0.0.1")
+    terminals = [IPAddr("10.0.0.6"),IPAddr("10.0.0.7"),IPAddr("10.0.0.8")]
+    data = {"edges":primary_edges,"mcast_address":IPAddr("10.10.10.10"),"root":root,"terminals":terminals,"adjacency":None,"controller":None}   
+    primary = PrimaryTree(**data)
+    
+
+    data = {"primary_tree":primary,"edges":backup_edges,"backup_edge":(1,2),"root":root,"terminals":terminals,"adjacency":None,"controller":None,"mcast_address":IPAddr("10.0.0.7")}    
+    backup = BackupTree(**data)
+    
+    primary.backup_trees.append(backup)
+
+    #result = backup.compute_nodes_to_signal((2,3))
+    
+    expected_result = [10,9,1]
+    
+    print "test_bottom_up_signal_trees2() result = %s" %(backup.nodes_to_signal)
+    print backup.nodes_to_signal
+    self.assertEquals(backup.nodes_to_signal, expected_result)
+    
+    
+    
+##############################################################   DEPRACTED TESTS BELOW    ######################################################################################
+    
+  def depracted_test_find_nodes_to_signal(self):
     
     primary_edges = [(1,2),(2,3),(3,4),(5,6)]
     backup_edges = [(1,2),(2,4),(4,5),(5,6),(6,7)]
     
-    backup = Tree()
+    backup = BackupTree()
     backup.edges = backup_edges
     primary = PrimaryTree()
     primary.edges = primary_edges
@@ -38,10 +118,9 @@ class BackupTreeInstallerTests(unittest.TestCase):
     expected_result = [2,4,6]
     
     self.assertEquals(result, expected_result)
-    
         
     
-  def test_find_affected_trees(self):
+  def depracted_test_find_affected_trees(self):
     """ Find and return the multicast address associate with the primary trees using the failed link 
     
     Keyword Arguments:
